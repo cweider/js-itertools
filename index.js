f14d9b86b1745be3df4fb3d4fbff6459f34e9dfa
Chad Weider <cweider@oofn.net>
2012-05-04 18:31:14 -0700

    Add index for module.

---
+++ index.js
@@ -0,0 +1,11 @@
+/*
+    index.js
+
+    Created by Chad Weider
+
+    Released into public domain on 10/10/2010.
+    NO WARRANTY EXPRESSED OR IMPLIED. USE AT YOUR OWN RISK.
+
+*/
+
+exports = require('./itertools');
